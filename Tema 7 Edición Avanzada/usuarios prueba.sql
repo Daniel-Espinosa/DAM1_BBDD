47b481fa1a41ac76efe04a23c25c266553c26cb4
Daniel-Espinosa <anareldis@gmail.com>
2023-05-12 17:56:28 +0200

    Crear Usuarios BBDD

---
+++ Tema 7 Edición Avanzada/usuarios prueba.sql
@@ -0,0 +1,7 @@
+Create user 'albano'@'%' identified by '1234';
+grant all on *.* to 'albano'@'%' with grant option;
+drop user 'albano';
+
+Create user 'carmen'@'%' identified by '1234';
+grant all on *.* to 'carmen'@'%' with grant option;
+drop user 'carmen';
